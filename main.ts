c12f610e4b5f60b13e2be0deddf94f3e1a9962d5
Domagoj <paulic.domagoj@gmail.com>
2024-04-05 12:38:24 +0200

    create custom pipeline

--- main.ts
+++ main.ts
@@ -21,3 +21,50 @@ const response = await queryEngine.query({
 });
 
 console.log({ response: response.toString() });
+
+import * as llamaIndex from 'npm:llamaindex@0.1.8';
+
+let customLLM = new llamaIndex.OpenAI();
+let customEmbedding = new llamaIndex.OpenAIEmbedding();
+
+let customServiceContext = new llamaIndex.serviceContextFromDefaults({
+  llm: customLLM,
+  embedModel: customEmbedding,
+});
+
+let customQaPrompt = function ({ context = '', query = '' }) {
+  return `Context information is below.
+      ---------------------
+      ${context}
+      ---------------------
+      Given the context information, answer the query.
+      Include a random fact about whales in your answer.\
+      The whale fact can come from your training data.
+      Query: ${query}
+      Answer:`;
+};
+
+let customResponseBuilder = new llamaIndex.SimpleResponseBuilder(
+  customServiceContext,
+  customQaPrompt
+);
+
+let customSynthesizer = new llamaIndex.ResponseSynthesizer({
+  responseBuilder: customResponseBuilder,
+  serviceContext: customServiceContext,
+});
+
+let customRetriever = new llamaIndex.VectorIndexRetriever({
+  index,
+});
+
+let customQueryEngine = new llamaIndex.RetrieverQueryEngine(
+  customRetriever,
+  customSynthesizer
+);
+
+let response2 = await customQueryEngine.query({
+  query: 'What does the author think of college?',
+});
+
+console.log(response2.toString());
