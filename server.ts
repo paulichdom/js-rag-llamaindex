6e0db536a4a44fbcd6e864bc13015b95672a96c2
Domagoj <paulic.domagoj@gmail.com>
2024-04-07 11:49:03 +0200

    update server with a prompt

--- server.ts
+++ server.ts
@@ -1,7 +1,31 @@
-const handler = (req) => {
-  // Create a new response object
-  const body = new TextEncoder().encode("Hello World!");
-  return new Response(body, { status: 200 });
-};
+import * as mod from 'https://deno.land/std@0.213.0/dotenv/mod.ts';
+import {
+  Document,
+  VectorStoreIndex,
+  SimpleDirectoryReader,
+} from 'npm:llamaindex@0.1.8';
+const keys = await mod.load({ export: true }); // read API key from .env
 
-let server = Deno.serve({ port: 8001 }, handler);
+const documents = await new SimpleDirectoryReader().loadData({
+  directoryPath: './data',
+});
+const index = await VectorStoreIndex.fromDocuments(documents);
+const queryEngine = index.asQueryEngine();
+
+const handler2 = async (req) => {
+  if (req.method == 'POST') {
+    // we'll expect the incoming query to be a JSON object of the form {query: ...}
+    let data = await req.json();
+    let answer = await queryEngine.query({ query: data.query });
+    // and our response will be a JSON object of the form {response: ...}
+    let responseObj = {
+      response: answer.toString(),
+    };
+    return new Response(JSON.stringify(responseObj), {
+      status: 200,
+    });
+  } else {
+    return new Response('Not found', { status: 404 });
+  }
+};
+let server2 = Deno.serve({ port: 8002 }, handler2);
